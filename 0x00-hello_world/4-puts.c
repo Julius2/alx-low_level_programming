ec23d19b5ab2f655771e77c0a0ccae49040b137d
julius2 <julius.lancelot@highways.gov.gh>
2023-02-16 13:23:35 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Write a C program using puts function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
