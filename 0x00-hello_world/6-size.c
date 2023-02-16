6bfb813821ac8dd56477c7ccb2361bfb66e61b90
julius2 <julius.lancelot@highways.gov.gh>
2023-02-16 14:47:19 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,12 +5,10 @@
  */
 int main(void)
 {
-printf("The size of char is %lu bytes\n", sizeof(char));
-printf("The size of int is %lu bytes\n", sizeof(int));
-printf("The size of long is %lu bytes\n", sizeof(long int));
-printf("The size of long long is %lu bytes\n", sizeof(long long int));
-printf("The size of float is %lu bytes\n", sizeof(float));
+printf("Size of char: %lu byte(s)\n", sizeof(char));
+printf("Size of int: %lu byte(s)\n", sizeof(int));
+printf("Size of long: %lu byte(s)\n", sizeof(long int));
+printf("Size of long long int: %lu byte(s)\n", sizeof(long long int));
+printf("Size of float: %lu byte(s)\n", sizeof(float));
+return (0);
 }
-
-
-
