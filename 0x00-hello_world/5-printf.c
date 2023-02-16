d131f8da911deaffd9d1b9726b6d422837d26630
julius2 <julius.lancelot@highways.gov.gh>
2023-02-16 13:35:51 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Write a C program that print a line using the printf
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+		return (0);
+}
+		
