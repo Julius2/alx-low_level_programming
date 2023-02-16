1f5f604f327b576ad8def5cc23ef899a1a51a59d
julius2 <julius.lancelot@highways.gov.gh>
2023-02-16 14:53:16 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - Write a C program that prints the size of various types on the computer it is compiled and run on
+ * main - Size is not grandeur, and territory does not make a nation
  * Return: 0 (Success)
  */
 int main(void)
