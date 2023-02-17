20fe0b1d953e1929094015b69d2544cc03ff363a
julius2 <julius.lancelot@highways.gov.gh>
2023-02-16 16:38:46 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A c program that prints a standard error
- *
- * return 1 (Success)
+ * main - Entry point
+ * Return: Always 0 (Sucess)
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stdout);
-return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
