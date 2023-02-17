56e5189b2802095e5a96d2195102f752de0c304a
julius2 <julius.lancelot@highways.gh>
2023-02-17 01:00:44 +0000

    all tasks completed

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Write a C program using puts function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
+
