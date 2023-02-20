e606a36ae68739bf2a3b452ccbb5f29e81eb97fd
julius2 <julius.lancelot@highways.gh>
2023-02-20 18:10:25 +0000

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,41 +8,28 @@
 
 int main(void)
 {
-	int c = 0;
-	int f_d;
-	int 1_d;
+	int i, j;
 
-	int c2;
-	int f_d2;
-	int 1_d2;
-
-	while (c <= 98)
+	for (i = 0; i < 100; i++)
 	{
-		f_d = (c / 10 + '0');
-		1_d = (c % 10 + '0');
-		c2 = 0;
-		while (c2 <= 99)
+		for (j = 0; j < 100; j++)
 		{
-			f_d2 = (c2 / 10 + '0');
-			1_d2 = (c2 % 10 + '0');
-
-			if  (c < c2)
+			if (i < j)
 			{
-				putchar(f_d);
-				putchar(1_d);
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
 				putchar(' ');
-				putchar(f_d2);
-				putchar(1_d2);
-
-				if (c != 98)
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
+				if (i != 98 || j != 99)
 				{
 					putchar(',');
 					putchar(' ');
 				}
 			}
-			c2++;
+
 		}
-		c++;
+	
 	}
 	putchar('\n');
 	return (0);
