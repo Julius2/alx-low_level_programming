cc25a5428df338e0f1d3a3c2f3921c03634fec4e
julius2 <julius.lancelot@highways.gh>
2023-02-22 05:37:44 +0000

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -21,10 +21,10 @@ int main(void)
 		fib1 = fib2;
 		fib2 = sum;
 	}
-	fib1_half1 = fib1 / 100000000000;
-	fib2_half1 = fib2 / 100000000000;
-	fib1_half2 = fib1 % 100000000000;
-	fib2_half2 = fib2 % 100000000000;
+	fib1_half1 = fib1 / 10000000000;
+	fib2_half1 = fib2 / 10000000000;
+	fib1_half2 = fib1 % 10000000000;
+	fib2_half2 = fib2 % 10000000000;
 	for (count = 93; count < 99; count++)
 	{
 		half1 = fib1_half1 + fib2_half1;
@@ -32,7 +32,7 @@ int main(void)
 		if (fib1_half2 + fib2_half2 > 9999999999)
 		{
 			half1 += 1;
-			half2 %= 100000000000;
+			half2 %= 10000000000;
 		}
 		printf("%lu%lu", half1, half2);
 		if (count != 98)
