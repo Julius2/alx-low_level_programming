874dfeccba7e69322b6b2728e194c9e8939a7bc9
julius2 <julius.lancelot@highways.gh>
2023-02-22 00:40:51 +0000

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute vslue of an integer.
+ * @c: The number tobe computed.
+ * Return: Absolute value of a number or zero
+ */
+
+int _abs(int c)
+
+{
+	if (c < 0)
+	{
+	int abs_val;
+
+	abs_val = c * -1;
+	return (abs_val);
+	}
+	return (c);
+}
