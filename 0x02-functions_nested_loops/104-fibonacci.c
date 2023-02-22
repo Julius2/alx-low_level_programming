ad772e7c34a627958922b805f7d4565e06af7326
julius2 <julius.lancelot@highways.gh>
2023-02-22 05:34:24 +0000

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -38,7 +38,7 @@ int main(void)
 		if (count != 98)
 			printf(", ");
 		fib1_half1 = fib2_half1;
-			fib1_hal2 = fib2_half2;
+		fib1_half2 = fib2_half2;
 		fib2_half1 = half1;
 		fib2_half2 = half2;
 	}
