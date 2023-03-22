f7175bfb68487f6b4c9e5430246f0246a7e89e83
julius2 <julius.lancelot@highways.gh>
2023-03-22 22:46:48 +0000

    0-print_name.c

--- 0x0F-function_pointers/0-main.c
+++ 0x0F-function_pointers/0-main.c
@@ -37,3 +37,15 @@ void print_name_uppercase(char *name)
         i++;
     }
 }
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_name("Bob", print_name_as_is);
+    print_name("Bob Dylan", print_name_uppercase);
+    printf("\n");
+    return (0);
+}
